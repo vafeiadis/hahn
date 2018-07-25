b67f8f4eccc8edab2fab8933a2a383a3c13cea79
Viktor Vafeiadis <viktor@mpi-sws.org>
2018-07-25 23:38:03 +0200

    More lemmas about segments

--- Hahn.v
+++ Hahn.v
@@ -1,6 +1,7 @@
 Require Export HahnBase HahnFun HahnList HahnSets.
 Require Export HahnRelationsBasic HahnEquational.
-Require Export HahnMaxElt HahnMinElt HahnRewrite HahnPath.
+Require Export HahnMaxElt HahnMinElt HahnRewrite.
+Require Export HahnPath HahnAcyclic.
 Require Export HahnDom HahnFuneq HahnWf.
 Require Export HahnSorted HahnSegment. 
 Require Export HahnTotalExt HahnTotalList.
--- HahnSegment.v
+++ HahnSegment.v
@@ -1,5 +1,5 @@
 (******************************************************************************)
-(** * Lemmas about starting segments of finitely-supported total orders *)
+(** * Lemmas about segments of finitely-supported total orders *)
 (******************************************************************************)
 
 Require Import HahnBase HahnSets HahnList HahnRelationsBasic HahnWf HahnMinElt.
@@ -17,10 +17,17 @@ Proof.
   destruct NMIN; desf; eauto.
 Qed.
 
+Section Segments.
+
+Variable A : Type.
+Variable cond : A -> Prop.
+Variable r : relation A.
+Variable STO : strict_total_order cond r.
+Variable FSUPP: fsupp r.
+
 (** Every finitely-supported total order has a starting segment *)
 
-Lemma starting_segment_exists A cond (r: relation A) 
-    (STO: strict_total_order cond r) (FSUPP: fsupp r) x (Cx: cond x):
+Lemma starting_segment_exists x (Cx: cond x):
   exists l, << SORTED: StronglySorted r l >> /\ 
             << INIT: forall x', cond x' /\ r^* x' x <->  In x' l >>.
 Proof.
@@ -48,78 +55,83 @@ Proof.
     destruct (N x'); vauto. 
 Qed.
 
-Lemma starting_segment_exists_unique A cond (r: relation A) 
-    (STO: strict_total_order cond r) (FSUPP: fsupp r) x (Cx: cond x):
+Lemma starting_segment_exists_unique x (Cx: cond x):
   exists! l, << SORTED: StronglySorted r l >> /\ 
              << INIT: forall x', cond x' /\ r^* x' x <->  In x' l >>.
 Proof.
-  assert (X:=starting_segment_exists STO FSUPP x Cx); desc.
+  assert (X:=starting_segment_exists Cx); desc.
   destruct STO.
   exists l; split; ins; desc.
   eapply StronglySorted_eq; eauto.
   by ins; rewrite <- INIT, INIT0.
 Qed.
 
-Definition starting_segment A cond R STO FSUPP x Cx := 
+Definition starting_segment x Cx := 
    proj1_sig (constructive_definite_description _ 
-                (@starting_segment_exists_unique A cond R STO FSUPP x Cx)).
-   
-Lemma starting_segment_property A cond R STO FSUPP x Cx:
-  << SORTED: StronglySorted R  (@starting_segment A cond R STO FSUPP x Cx) >> /\ 
-  << INIT: forall x', cond x' /\ R^* x' x <->  In x' (@starting_segment A cond R STO FSUPP x Cx) >>.
+                (@starting_segment_exists_unique x Cx)).
+
+Lemma starting_segment_property x (Cx: cond x) : 
+  << SORTED: StronglySorted r (starting_segment Cx) >> /\ 
+  << INIT: forall x', cond x' /\ r^* x' x <->  In x' (starting_segment Cx) >>.
 Proof.
   unfold starting_segment; match goal with [ |- context[proj1_sig ?t] ] => destruct t end; 
     eauto.
 Qed.
 
-Lemma starting_segment_contains_endpoint A cond R STO FSUPP x Cx:
-  In x (@starting_segment A cond R STO FSUPP x Cx).
+Lemma in_starting_segment_iff x (Cx: cond x) z :
+  In z (starting_segment Cx) <-> cond z /\ r＊ z x.
 Proof.
-  apply starting_segment_property; vauto.
+  symmetry; apply starting_segment_property.
 Qed.
 
-Lemma start_of_starting_endpoint A cond R STO FSUPP x Cx:
-  exists l, @starting_segment A cond R STO FSUPP x Cx = l ++ x :: nil.
+Lemma starting_segment_contains_endpoint x (Cx: cond x) :
+  In x (starting_segment Cx).
 Proof.
-  assert (X:=starting_segment_contains_endpoint STO FSUPP x Cx).
+  apply in_starting_segment_iff; vauto.
+Qed.
+
+Lemma start_of_starting_endpoint x (Cx: cond x) :
+  exists l, starting_segment Cx = l ++ x :: nil.
+Proof.
+  assert (X:=starting_segment_contains_endpoint Cx).
   apply in_split in X; desf.
   enough (l2 = nil) by (desf; eauto). 
-  assert (Y:=starting_segment_property STO FSUPP x Cx); desf.
+  assert (Y:=starting_segment_property Cx); desf.
   rewrite X in *; clear X.
   apply StronglySorted_app_iff, proj2, proj1 in SORTED.
   apply StronglySorted_cons_iff, proj2 in SORTED.
   destruct l2; ins.
   rewrite Forall_cons in *.
-  assert (R^* a x) by (apply INIT; eauto with hahn).
-  do 2 (red in STO; desc). 
-  exfalso; eapply STO, ct_of_trans, ct_end; vauto.
+  assert (r^* a x) by (apply INIT; eauto with hahn).
+  destruct STO as [[IRR T] TOT]. 
+  exfalso; eapply IRR, ct_of_trans, t_rt_step; desf; vauto.
 Qed.  
 
-Lemma NoDup_starting_segment A cond R STO FSUPP x Cx:
-  NoDup (@starting_segment A cond R STO FSUPP x Cx).
+Lemma NoDup_starting_segment x (Cx: cond x) :
+  NoDup (starting_segment Cx).
 Proof.
-  do 2 (red in STO; desc).
+  destruct STO as [[IRR T] TOT]. 
   eapply NoDup_StronglySorted; eauto. 
   apply starting_segment_property.
 Qed.
 
-Lemma not_in_starting_segment A (cond: A -> Prop) R STO FSUPP x Cx y (Cy: cond y):
-  ~ In y (@starting_segment A cond R STO FSUPP x Cx) -> R x y.
+Lemma not_in_starting_segment x (Cx: cond x) y (Cy: cond y):
+  ~ In y (starting_segment Cx) -> r x y.
 Proof.
   ins; apply NNPP; intro.
   destruct H; apply starting_segment_property.
   split; ins.
-  red in STO; desf.
-  specialize (STO0 _ Cx _ Cy).
-  apply imply_to_or in STO0; desf; vauto.
-  apply NNPP in STO0; desf; vauto.
+  destruct STO as [[IRR T] TOT]. 
+  specialize (TOT _ Cx _ Cy).
+  apply imply_to_or in TOT; desf; clarify_not; vauto.
 Qed.
 
 
+
 (* NB: Cy is not necessary, but it is easier to use the lemma with it. *)
-Lemma start_of_starting_segment A cond R STO FSUPP x Cx y Cy l l':
-   @starting_segment A cond R STO FSUPP x Cx = l ++ y :: l'
-   -> @starting_segment A cond R STO FSUPP y Cy = l ++ y :: nil.
+Lemma start_of_starting_segment x (Cx : cond x) y (Cy : cond y) l l':
+   starting_segment Cx = l ++ y :: l'
+   -> starting_segment Cy = l ++ y :: nil.
 Proof.
   assert (T := STO); do 2 (red in T; desc).
   ins; eapply StronglySorted_eq; vauto.
@@ -130,7 +142,7 @@ Proof.
     by apply starting_segment_property.
   * split; ins.
     + apply starting_segment_property in H0; desc.
-      specialize (starting_segment_property STO FSUPP x Cx); ins; desf; rewrite H in *.
+      specialize (starting_segment_property Cx); ins; desf; rewrite H in *.
       forward apply (proj1 (INIT x0)) as X.
       { split; ins.
         eapply rt_trans; eauto.
@@ -146,7 +158,7 @@ Proof.
       apply (ct_of_trans T1) in H1.
       eby eapply T, T1.
     + apply starting_segment_property.
-      specialize (starting_segment_property STO FSUPP x Cx); ins; desf; rewrite H in *.
+      specialize (starting_segment_property Cx); ins; desf; rewrite H in *.
       forward apply (proj2 (INIT x0)) as X.
         by rewrite in_app_iff in H0; ins; desf; eauto with hahn.
       split; desf.
@@ -156,3 +168,50 @@ Proof.
       apply rt_step; eauto with hahn.
 Qed.
 
+Definition segment x (Cx : cond x) y (Cy : cond y) :=
+  filterP (fun e => ~ In e (starting_segment Cx)) (starting_segment Cy).
+
+Lemma segment_property x (Cx: cond x) y (Cy: cond y):
+  ⟪SORTED: StronglySorted r (segment Cx Cy)⟫ /\
+  ⟪SEGMENT: forall z, cond z /\ r x z /\ r＊ z y <-> In z (segment Cx Cy)⟫.
+Proof.
+  ins; splits.
+    by apply StronglySorted_filterP, starting_segment_property.
+  split; ins; desf.
+  * apply in_filterP_iff; split.
+      by apply starting_segment_property.
+    intro N; apply starting_segment_property in N; desf.
+    destruct STO as [[IRR T] TOT]. 
+    apply clos_refl_transE in N0; desf; eauto. 
+    apply clos_trans_of_transitiveD in N0; eauto.
+  * unfold segment in H; apply in_filterP_iff in H; desf.
+    apply starting_segment_property in H; desf; splits; try done.
+    eby eapply not_in_starting_segment.
+Qed.
+
+Lemma in_segment_iff x (Cx: cond x) y (Cy: cond y) z :
+  In z (segment Cx Cy) <-> cond z /\ r x z /\ r^* z y.
+Proof.
+  symmetry; apply segment_property.
+Qed.
+
+Lemma starting_segment_decomp x (Cx: cond x) y (Cy: cond y) (Rxy: r^* x y):
+  starting_segment Cy = starting_segment Cx ++ segment Cx Cy.
+Proof.
+  destruct STO as [[IRR T] TOT]. 
+  eapply StronglySorted_eq; eauto.
+  * by red; eauto.
+  * by apply starting_segment_property.
+  * apply StronglySorted_app; ins.
+      by apply starting_segment_property.
+      by apply segment_property.
+    apply starting_segment_property in IN1; apply segment_property in IN2; desf.
+    rewrite clos_refl_transE in *; desf; eauto.
+    all: repeat match goal with H : _⁺ _ _ |- _ => apply (ct_of_trans T) in H end; eauto.
+  * ins; rewrite in_app_iff, !in_starting_segment_iff, in_segment_iff.
+    intuition; eauto using clos_refl_trans.
+    tertium_non_datur (x0 = x) as [->|NEQ]; eauto using clos_refl_trans.
+    eapply TOT in NEQ; desf; eauto using clos_refl_trans.
+Qed.
+
+End Segments.
